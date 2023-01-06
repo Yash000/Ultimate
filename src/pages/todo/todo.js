341aec3f3aebd7b374eda53f4aea0d16ed35b020
Yash Baidya <yashkrbaidya@gmail.com>
2023-01-06 20:25:11 +0530

    ..

--- src/pages/todo/todo.js
+++ src/pages/todo/todo.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import './todo.css'
+import {Button,Card,InputGroup,Form} from 'react-bootstrap';
+
+
 
 const Todo = () => {
 
-    const [text,setText]=React.useState({
-        add:""
+    const [text, setText] = React.useState({
+        add: ""
     })
 
-    const [items,setItems]=React.useState([""])
-    function handleClick(){
-        setItems(x=>([...x,text.add]))
+    const [items, setItems] = React.useState([])
+    function handleClick() {
+        setItems(x => (text.add==""?[...x]: [...x, text.add]))
     }
-    const showItems=items.map(x=><h2>{x}</h2>)
+    const showItems = items.map(x => <Card body>{x}</Card>)
 
-    
-    function handleChange(event){
-        setText(x=>({
+
+    function handleChange(event) {
+        setText(x => ({
             ...x,
-            [event.target.name]:event.target.value
+            [event.target.name]: event.target.value
         }))
     }
 
@@ -25,15 +28,19 @@ const Todo = () => {
         <div className='Main'>
             Todo
             <br />
-            <input type="text"
+            <InputGroup className="mb-3">
+            
+            <Form.Control type="text"
                 placeholder="type items"
                 width="50%"
                 name="add"
                 value={text.value}
                 onChange={handleChange}
-            />
-            <br />
-            <input type="submit" value="Add item" onClick={handleClick}/>
+                id="type_item"
+                onKeyUp={(e) => { if (e.key === "Enter") { handleClick(); } }}
+                />
+            <Button type="submit" value="Add item" onClick={handleClick} id="add_item" variant="light">+</Button>
+            </InputGroup>
             <div>{showItems}</div>
         </div>
     )
